f2bb44dfa957afab65b432ac34b7c442a5be36df
jiyeonhae <bjy51188@gmail.com>
2022-09-26 12:24:44 +0900

    1.1

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -7,9 +7,8 @@ public class Main {
         * 주석 : 여러줄의 영역을 인식하지 못하게 해야할 경우에 사용
         */
 
-        int i = 10;
-
-        System.out.println("Hello world!");
-        System.out.println("홍길동");
+       int a = 2;
+       System.out.print(a);
+        //aaaaaa
     }
 }
