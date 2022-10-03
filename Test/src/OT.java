a5f4b329afd44e086e1ad89b36b3e23b34a2cbd6
jiyeonhae <bjy51188@gmail.com>
2022-10-04 01:28:11 +0900

    1004

--- Test/src/OT.java
+++ Test/src/OT.java
@@ -1,5 +1,20 @@
 public class OT {
     public static void main(String[] args) {
+        //구구단
+        {
+            for (int n = 1; n < 10; n++) {
+                int dan = 2;
+                while (dan < 10) {
+                    System.out.println(dan * n);
+                    dan++;
+                    }
+            }
+
+        }
+        //구구단 홀수 or 짝수단
 
     }
 }
+
+
+
