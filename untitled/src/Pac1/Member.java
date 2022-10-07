69212160ce377ebdbc00389c3d17ffad7462672e
jiyeonhae <bjy51188@gmail.com>
2022-10-07 13:19:58 +0900

    1007.2

--- untitled/src/Pac1/Member.java
+++ untitled/src/Pac1/Member.java
@@ -4,8 +4,23 @@ public class Member {
     //getter 참조
     //setter 변경
 
+    // 객체지향의 4단계 = initialize 생성 progress 변경 render(update) 갱신 release(delete) 삭제
+
     public String id;
     private String userName;
+    private Integer age;
+/////////////////////////////////////////////////data
+    //생성자
+    //메서드
+    public String getId() {return id;}
+    void setId(String id) { this.id = id;}
 
+    public String getUserName() {return userName;}
+    void setUserName(String userName) {this.userName = userName;}
 
+    public Integer getAge() {return age;} //code generate getter/setter로 성성
+    public void setAge(Integer age) {this.age = age;}
+    
+    //접근제어자 반환값의데이터타입 함수이름 (매개변수 파라미터 -- 지역변수임) { return 반환값;}
+//////////////////////////////////////function          data + function = capsule화
 }
---
+++ untitled/src/Pac1/MemberMain.java
@@ -0,0 +1,53 @@
+package Pac1;
+
+public class MemberMain {
+    
+
+    public static void main(String[] args) {
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!! stack hip 등 data 구조 정리해오기!!!!!!!!!!!!!!!!!!!
+
+        Member m = new Member();
+        Member n = new Member();
+        //stack 동 ---new는 heap 연산자
+        //member라는 zone에 자리가 들어갈것이다 표시 -- heap에 Member m or n의 값이 결정되면 자리부여됨 -- 같은 클래스 다른 주소
+
+        m.id = "id111";
+        n.id = "id222";
+
+
+
+        if (m == n) {
+            System.out.println("same");
+        }
+        else System.out.println("different"); //id 가 아니더라도 걍 주소가 달라서 다름으로 결과나옴
+
+
+
+
+
+
+        Member[] mArray = new Member[3];  //클래스를 배열구조로 사용하기
+        mArray[0] = new Member();
+        mArray[1] = new Member();
+        mArray[2] = new Member(); // initialize
+
+
+        mArray[0].setId("id0");
+        mArray[1].setId("id1");
+        mArray[2].setId("id2");  //set value
+
+
+        for (Member element: mArray) {
+            String str = element.getId(); //return value
+            System.out.println(str);
+        }
+
+
+
+
+
+
+
+
+    }
+}
